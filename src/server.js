e1ccfc7fefc46c5853345c7d02c6aecf7927fd12
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-08-26 21:19:07 +0200

    URL parameters hander added

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-//Middelware to handle post requst
+//Middelware to handle post requst as JSON
 app.use(express.json());
 
 //Behavior what to do
@@ -14,6 +14,16 @@ app.post('/hello', (req, res) => {
     console.log('post request bellow');
     console.log(req.body);
     res.send(`hello, this is a POST response for "${req.body.name}"`);
+});
+
+//URL parameters
+app.get('/hello/:name', (req, res) => {
+    //Request
+    const { name } = req.params; //Object destructuring //const name = req.params.name;
+    console.log(`URL parameters was ${name}`);
+
+    //Response
+    res.send(`Your URL parameter is "${name}"`);
 });
 
 app.put('/hello', (req, res) => {

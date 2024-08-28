33e70e48afe4fd5d714d136449c40ce774d6041b
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-08-28 20:14:40 +0200

    Post request handler with json objects

--- src/server.js
+++ src/server.js
@@ -1,28 +1,46 @@
 import express from 'express';
 
-const app = express();
-
-//Middelware to handle post requst as JSON
-app.use(express.json());
-
-//Upvote endpoint
 let articleInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-mongodb',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }];
+
+const app = express();
+app.use(express.json()); //Middelware to handle post requst as JSON
+
+app.listen(8000, () => {
+    console.log('Server is listning on port 8000');
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const article = articleInfo.find(a => a.name === name);
+
+    if (article) {
+        const { postedBy, text } = req.body;
+        console.log(articleInfo.find(a => a.name === name).comments);
+        article.comments.push({ postedBy, text});
+        res.send(article.comments);
+    } else {
+        res.send(`There is no article with name ${name}.`)
+    }
+});
 
 app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
     const article = articleInfo.find(a => a.name === name); //Its not a cp but passed by reference
     //articleInfo.find(a => a.name === name).upvotes++;
     if (article) {
-        article.upvotes++;
+        article.upvotes += 1;
         res.send(`The article has now ${article.upvotes} votes.`)
     } else {
         res.send(`There is no article with name ${name}.`)
@@ -30,33 +48,8 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     console.log(articleInfo);
 });
 
-//Behavior what to do
-app.get('/hello', (req, res) => {
-    res.send('hello gowsigan');
-});
-
 app.post('/hello', (req, res) => {
     console.log('post request bellow');
     console.log(req.body);
     res.send(`hello, this is a POST response for "${req.body.name}"`);
 });
-
-//URL parameters
-app.get('/hello/:name', (req, res) => {
-    //Request
-    const { name } = req.params; //Object destructuring //const name = req.params.name;
-    console.log(`URL parameters was ${name}`);
-
-    //Response
-    res.send(`Your URL parameter is "${name}"`);
-});
-
-app.put('/hello', (req, res) => {
-    res.send('hello gowsigan this is a PUT response');
-});
-
-//Tell ower server to listen
-app.listen(8000, () => {
-    console.log('Server is listning on port 8000');
-});
-

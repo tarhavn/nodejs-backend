098ab21e67aee1776c14d2d96cff02780c676a35
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-09-02 18:57:52 +0200

    Adding Mongodb to Express

--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
 import express from 'express';
-
-let articleInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'learn-mongodb',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-}];
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(express.json()); //Middelware to handle post requst as JSON
 
 app.listen(8000, () => {
     console.log('Server is listning on port 8000');
+});
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+    const db = client.db('react-blog-db');
+    const article = await db.collection('articles').findOne({ name });
+    if (article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articleInfo.find(a => a.name === name); //Its not a cp but passed by reference
+    //articleInfo.find(a => a.name === name).upvotes++;
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The article has now ${article.upvotes} votes.`)
+    } else {
+        res.send(`There is no article with name ${name}.`)
+    }
+    console.log(articleInfo);
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {
@@ -33,23 +47,4 @@ app.post('/api/articles/:name/comments', (req, res) => {
     } else {
         res.send(`There is no article with name ${name}.`)
     }
-});
-
-app.put('/api/articles/:name/upvote', (req, res) => {
-    const { name } = req.params;
-    const article = articleInfo.find(a => a.name === name); //Its not a cp but passed by reference
-    //articleInfo.find(a => a.name === name).upvotes++;
-    if (article) {
-        article.upvotes += 1;
-        res.send(`The article has now ${article.upvotes} votes.`)
-    } else {
-        res.send(`There is no article with name ${name}.`)
-    }
-    console.log(articleInfo);
-});
-
-app.post('/hello', (req, res) => {
-    console.log('post request bellow');
-    console.log(req.body);
-    res.send(`hello, this is a POST response for "${req.body.name}"`);
 });

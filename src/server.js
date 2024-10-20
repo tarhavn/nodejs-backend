d278ef16b4d297b077391a858af2dd4d2ae92a5f
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-10-20 18:33:48 +0200

    Comment endpoint protected for only logged in users

--- src/server.js
+++ src/server.js
@@ -74,10 +74,13 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
+
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: {postedBy, text} }
+        $push: { comments: {postedBy: email, text} }
     });
+    
     const article = await db.collection('articles').findOne({ name });
     if (article) {
         res.json(article);
